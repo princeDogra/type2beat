b629539da3c2315a857805ba662c3ddb5939f56c
Prince Dogra <princedogra.au@gmail.com>
2020-04-27 05:47:44 +1000

    commit iteration 1 27/04/2020

---
+++ awareness/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+def exercises(request):
+    return render(request, 'exercises.html')
+
+def healthy_diets(request):
+    return render(request, 'healthy_diets.html')
