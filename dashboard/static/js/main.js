94d05c0aa1e59c04b15e11a7d46cb2d8bacec4be
student <pdog0001@student.monash.edu>
2020-05-14 05:16:23 +1000

    Berlin: commit for iteration 1 : glucose record 14/05/2020

--- dashboard/static/js/main.js
+++ dashboard/static/js/main.js
@@ -14,3 +14,44 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 })
+
+// =============================================================================
+// loading food data from the database
+// =============================================================================
+
+let foodData = []
+
+document.querySelector('#search-food-item').addEventListener('click', (e)=> {
+  let searchItem  = document.querySelector('#searchItem').value;
+  const ENDPOINT = "/api/data/food/?search="+searchItem;
+  $.ajax({
+    method: "GET",
+    url: ENDPOINT,
+    success: function(data){
+      foodData = data
+      displayData()
+    },
+    error: function(error_data){
+      console.log(error_data)
+    }
+  })
+})
+let displayData = () => {
+  let parent = document.querySelector('#food-item-holder');
+  let projector=document.querySelector('#projector');
+  let counter = 0;
+  let dict  =  new Object();
+  foodData.forEach((element)=>{
+    let childNode = document.createElement('li');
+    let textnode = document.createTextNode(element.product_name);
+    childNode.appendChild(textnode);
+    childNode.id = counter;
+    childNode.addEventListener('click', (e)=>{
+      projector.innerHTML = element.product_name;
+    });
+    parent.appendChild(childNode);
+    dict[''+counter]=element.id;
+    counter+=1;
+  });
+  console.log(dict);
+}
